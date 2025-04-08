69aad3d5bd2c45b6f16a0851eb7988f3634b8ca3
uri-git23 <saintname31@gmail.com>
2025-04-08 22:45:40 +0900

    언리얼 vs 유니티 교육세션 과제

---
+++ ValueCheck.cs
@@ -0,0 +1,25 @@
+﻿//Output: min and MAX of integral types
+Console.WriteLine("Signed integral types:");
+
+//Signed 8bit integral
+Console.WriteLine($"sbyte  : {sbyte.MinValue} to {sbyte.MaxValue}");
+//Signed 16bit integral
+Console.WriteLine($"short  : {short.MinValue} to {short.MaxValue}");
+//Signed 32bit integral
+Console.WriteLine($"int    : {int.MinValue} to {int.MaxValue}");
+//Signed 64bit integral
+Console.WriteLine($"long   : {long.MinValue} to {long.MaxValue}");
+
+Console.WriteLine("");
+Console.WriteLine("Unsigned integral types:");
+
+//Unsigned 8bit integral
+//Especially byte represents data's byte value
+//Needed: data incoding and decoding
+Console.WriteLine($"byte   : {byte.MinValue} to {byte.MaxValue}");
+//Unsigned 16bit integral
+Console.WriteLine($"ushort : {ushort.MinValue} to {ushort.MaxValue}");
+//Unsigned 32bit integral
+Console.WriteLine($"uint   : {uint.MinValue} to {uint.MaxValue}");
+//Unsigned 64bit integral
+Console.WriteLine($"ulong  : {ulong.MinValue} to {ulong.MaxValue}");
